dcfef47d07fe4183e1a4af4f529d6cc0826a33aa
oodwyer <orodwyer@gmail.com>
2020-07-30 19:09:26 -0400

    mobile styling fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Segment} from 'semantic-ui-react';
+import { Segment, Button } from 'semantic-ui-react';
 
 
 function Footer() {
   return <Segment basic id="footer-seg" vertical as="footer" textAlign="center">
+
     <div className="footer-text">
       Made with ♡ in Philadelphia by  {" "}
       <a className="footer-p" href="https://thesign.al">
-      The Signal </a>
-      .
+      The Signal. </a>
+      
       </div>
 
 
